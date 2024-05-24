205639a420c5672e91c3f98fac89d88c0640b664
TULIKA CHATTERJEE <tulika15082002@gmail.com>
2024-05-25 00:44:49 +0530

    6th

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,10 +10,10 @@ const Hero = () => {
       <div
         className={`absolute inset-0  top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start  gap-5`}
       >
-        <div className='flex flex-col justify-center items-center  mt-5'>
+        <div className='flex flex-col justify-center items-center  mt-6'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
+  </div>
 
         <div className="boxi display">
           <div>
@@ -26,11 +26,7 @@ const Hero = () => {
           </p>
           </div>
           
-          <img 
-           src={Profile} 
-           alt="Descriptive Alt Text" 
-          className="ml-4 rounded-dp-big display"
-           />
+          
         </div>
         <div>
 
